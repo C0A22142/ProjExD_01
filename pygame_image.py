6ee1c234c360b63600055a5774213c364740fda8
YamazakiMsy <c0a221424e@edu.teu.ac.jp>
2023-04-18 16:46:03 +0900

    途中4

--- pygame_image.py
+++ pygame_image.py
@@ -20,7 +20,7 @@ def main():
         x = tmr % 1600
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img, [1600 - x, 0])
-        screen.blit(kk_imgs[tmr%2], [300, 200])
+        screen.blit(kk_imgs[tmr%4], [300, 200])
 
         pg.display.update()
         clock.tick(100)
