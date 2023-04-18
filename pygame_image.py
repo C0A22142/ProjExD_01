08c4ee61a1565963471ed9ccbc16f531047e8cb4
YamazakiMsy <c0a221424e@edu.teu.ac.jp>
2023-04-18 17:11:44 +0900

    途中6

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    r_bg_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 3, 1.0), kk_img, pg.transform.rotozoom(kk_img, 3, 1.0)]
@@ -19,7 +20,7 @@ def main():
         tmr += 1
         x = tmr % 1600
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600 - x, 0])
+        screen.blit(r_bg_img, [1600 - x, 0])
         screen.blit(kk_imgs[tmr%4], [300, 200])
 
         pg.display.update()
